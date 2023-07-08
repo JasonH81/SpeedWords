00ddcb90595a2645a5a91b2d4c0c712961e3b48e
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2023-07-08 17:16:45 -0400

    Set up a Timer Panel & Added Scrollpane for text, Working on Gradle

--- src/jason/speedwords/SpeedWords.java
+++ src/jason/speedwords/SpeedWords.java
@@ -2,8 +2,15 @@ package jason.speedwords;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Insets;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -14,8 +21,11 @@ public class SpeedWords extends JFrame {
  
 	private static final long serialVersionUID = 1L;
 	private static final Color TAN = new Color(222,191,168);
+	private static final Font LIST_FONT = new Font(Font.DIALOG,Font.BOLD,14);
 	
 	private ScorePanel scorePanel = new ScorePanel(0,TAN);
+	private SpeedWordsTimerPanel swTimerPanel = new SpeedWordsTimerPanel(this,60);
+	private JTextArea textArea = new JTextArea();
 	
 	public SpeedWords() {
 		initGUI();
@@ -32,6 +42,44 @@ public class SpeedWords extends JFrame {
 	private void initGUI() {
 		TitleLabel titleLabel = new TitleLabel("Speed Words");
 		add(titleLabel,BorderLayout.PAGE_START);
+		
+		// main panel
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.X_AXIS));
+		mainPanel.setBackground(TAN);
+		add(mainPanel,BorderLayout.CENTER);
+		
+		// left panel
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel,BoxLayout.Y_AXIS));
+		leftPanel.setBackground(TAN);
+		mainPanel.add(leftPanel);
+		
+		// score panel
+		leftPanel.add(scorePanel);
+		
+		// timer panel
+		JPanel timerPanel = new JPanel();
+		timerPanel.setBackground(Color.RED);
+		leftPanel.add(timerPanel);
+		
+		swTimerPanel.setBackground(Color.RED);
+		timerPanel.add(swTimerPanel);
+		
+		
+		// game panel
+		
+		
+		// text area
+		Insets insets = new Insets(4,10,10,4);
+		textArea.setMargin(insets);
+		textArea.setEditable(false);
+		textArea.setFont(LIST_FONT);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		Dimension size = new Dimension(100,0);
+		scrollPane.setPreferredSize(size);
+		mainPanel.add(scrollPane);
 	}
 
 	public static void main(String[] args) {
---
+++ src/jason/speedwords/SpeedWordsTimerPanel.java
@@ -0,0 +1,19 @@
+package jason.speedwords;
+
+import java.awt.Font;
+
+import jason.notimer.NoTimerPanel;
+
+public class SpeedWordsTimerPanel extends NoTimerPanel {
+
+	private static final long serialVersionUID = 1L;
+	private static final Font FONT = new Font(Font.DIALOG,Font.BOLD,24);
+	
+	private SpeedWords speedWords;
+	
+	public SpeedWordsTimerPanel(SpeedWords speedWords, int time) {
+		super(time, FONT);
+		speedWords = this.speedWords;
+	}
+
+}
