044c398900dc911f8f1b232fb11b2840bc59cd0e
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2023-07-08 16:07:27 -0400

    Set Up Beginning of GUI and ScorePanel Dependencies

--- src/jason/speedwords/SpeedWords.java
+++ src/jason/speedwords/SpeedWords.java
@@ -1,12 +1,21 @@
 package jason.speedwords;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import jason.mycomponents.ScorePanel;
 import jason.mycomponents.TitleLabel;
 
 public class SpeedWords extends JFrame {
  
 	private static final long serialVersionUID = 1L;
+	private static final Color TAN = new Color(222,191,168);
+	
+	private ScorePanel scorePanel = new ScorePanel(0,TAN);
 	
 	public SpeedWords() {
 		initGUI();
@@ -22,11 +31,22 @@ public class SpeedWords extends JFrame {
 	
 	private void initGUI() {
 		TitleLabel titleLabel = new TitleLabel("Speed Words");
+		add(titleLabel,BorderLayout.PAGE_START);
 	}
 
 	public static void main(String[] args) {
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		}
+		catch (Exception e) {
+		}
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new SpeedWords();
+			}
+		});
 		
-
 	}
 
 }
