a931a4dd410f9b83bc38e0ca20b336e6bfb4d85e
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2023-07-08 15:43:38 -0400

    test

--- src/jason/speedwords/SpeedWords.java
+++ src/jason/speedwords/SpeedWords.java
@@ -2,12 +2,30 @@ package jason.speedwords;
 
 import javax.swing.JFrame;
 
+import jason.mycomponents.TitleLabel;
+
 public class SpeedWords extends JFrame {
  
 	private static final long serialVersionUID = 1L;
+	
+	public SpeedWords() {
+		initGUI();
+		
+		setTitle("Speed Words");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	private void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Speed Words");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
