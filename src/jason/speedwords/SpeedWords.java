c4bff635011cd8d91c8fce100526ce72bc9d852e
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2023-07-08 15:28:19 -0400

    Initial Commit

---
+++ src/jason/speedwords/SpeedWords.java
@@ -0,0 +1,14 @@
+package jason.speedwords;
+
+import javax.swing.JFrame;
+
+public class SpeedWords extends JFrame {
+ 
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
