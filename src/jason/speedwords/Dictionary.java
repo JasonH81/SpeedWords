0bbe7d771f70472fd5f1c7a69f82a28b64b682c7
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2024-08-27 22:18:38 -0400

    Coming back to an old project- added dictionary

---
+++ src/jason/speedwords/Dictionary.java
@@ -0,0 +1,12 @@
+package jason.speedwords;
+
+public class Dictionary {
+	private static final String FILE_NAME = "/enable1_2-7.txt";
+	private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static void main(String[] args) {
+		System.out.println(ALPHABET.length());
+
+	}
+
+}
